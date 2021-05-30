8666ea7af72669b6bf46616b8f453f6c2eb9b48e
tsukerov <tsukerovilja@gmail.com>
2021-05-30 22:18:50 +0300

    Before the unit "Create a more polished user experience"

--- dice_roller/src/main/java/ru/devtsukerov/dice_roller/MainActivity.kt
+++ dice_roller/src/main/java/ru/devtsukerov/dice_roller/MainActivity.kt
@@ -29,13 +29,29 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new two Dice object with 6 sides and roll it
         val dice = Dice(6)
+        val diceRoll = dice.roll()
             // Update the screen with the dice roll
 //        val resultTextView = findViewById<TextView>(R.id.textView)
 //        resultTextView.text = dice.roll().toString()
         val diceImage = findViewById<ImageView>(R.id.imageView)
-        diceImage.setImageResource(R.drawable.dice_3)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
+
+        }
+
 
     }
+
 
     class Dice(private val numSides: Int) {
 
@@ -43,4 +59,3 @@ class MainActivity : AppCompatActivity() {
             return (1..numSides).random()
         }
     }
-}
